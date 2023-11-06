8ce8795aff01c05b15842aea1ecd413db613c50a
JnnferJclyn <132757236+JnnferJclyn@users.noreply.github.com>
2023-11-06 13:23:17 +0700

    Jennifer Jocelyn
    Jennifer Jocelyn

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,6 +3,7 @@
 namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use App\Models\prodi;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -18,5 +19,26 @@ class DatabaseSeeder extends Seeder
         //     'name' => 'Test User',
         //     'email' => 'test@example.com',
         // ]);
+
+    prodi::create(
+        [
+            'name' => 'Tehnik Informatika'
+        ]
+    );
+
+     prodi::create(
+        [
+            'name' => 'Manajemen Informatika'
+        ]
+    );
+
+    prodi::create(
+        [
+            'name' => 'Sistem Informasi'
+        ]
+    );
     }
+
+   
+    
 }
